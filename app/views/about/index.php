2f5f03e6bdeedb2132bb369866b954a7ec8c4521
patriatbp <patria.tigabp2@gmail.com>
2020-08-19 13:46:37 +0700

    Membuat Asset dengan Bootstrap

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -4,7 +4,10 @@
 	<title></title>
 </head>
 <body>
+	<div class="container">
 	<h1>Patria</h1>
+	<img src="<?= BASEURL;  ?>/img/profile.jpg" alt="Patria" width="200" class="rounded-circle shadow">
 	<p>Halo, nama saya <?= $data['nama']  ?>, umur saya <?= $data['umur']  ?> tahun, saya seorang <?= $data['pekerjaan']  ?></p>
+	</div>
 </body>
 </html>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,6 +4,14 @@
 	<title></title>
 </head>
 <body>
-	<h1>Selamat Datang di Web saya</h1>
+	<div class="container">
+		<div class="jumbotron mt-5">
+		  <h1 class="display-4">Selamat Datang di Website saya</h1>
+		  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+		  <hr class="my-4">
+		  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+		  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+		</div>
+	</div>
 </body>
 </html>
